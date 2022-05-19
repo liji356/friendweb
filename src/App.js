30557d8e97addf808db076b5bedf5cd899211ec8
liji356 <lijibthomas95@gmail.com>
2022-05-19 15:52:02 +0530

    frndweb

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Header from './component/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     
     <>
-    <Header/>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" exact e
+    </Routes>
+    
+    </BrowserRouter>
     
     
     </>
